65885055440fc1938e5b5e7fd71f91e49ccc8dc0
josan-br <josantnun@outlook.com>
2022-04-12 17:56:54 -0300

    feat: basic request

---
+++ src/Http/Request.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace JosanBr\GalaxPay\Http;
+
+use GuzzleHttp\Client;
+use JosanBr\GalaxPay\Exceptions\AuthorizationException;
+use JosanBr\GalaxPay\Exceptions\GalaxPayRequestException;
+
+final class Request
+{
+    /**
+     * @var \GuzzleHttp\Client
+     */
+    private $client;
+
+    /**
+     * @var array
+     */
+    private $options;
+
+    /**
+     * @var array $options
+     */
+    public function __construct(array $options = null)
+    {
+        $this->options = $options;
+
+        $clientOptions = [
+            'debug' => $this->options['debug'],
+            'base_uri' => $this->options['baseUri'],
+            'headers' => ['Content-Type' => 'application/json']
+        ];
+
+        $this->client = new Client($clientOptions);
+    }
+
+    /**
+     * @param string $method
+     * @param string $route
+     * @param array $options
+     * @return array
+     * @throws \GuzzleHttp\Exception\ServerException
+     * @throws \GalaxPaySdkPhp\Exceptions\GalaxPayRequestException
+     * @throws \GalaxPaySdkPhp\Exceptions\AuthorizationException
+     */
+    public function send(string $method, string $route, array $options = [])
+    {
+        try {
+            if (isset($this->options['headers'])) {
+                foreach ($this->options['headers'] as $key => $value) {
+                    $options['headers'][$key] = $value;
+                }
+            }
+
+            $response = $this->client->request($method, $route, $options);
+
+            return json_decode($response->getBody()->getContents(), true);
+        } catch (\GuzzleHttp\Exception\RequestException $e) {
+            $status = $e->getResponse()->getStatusCode();
+            $data = json_decode($e->getResponse()->getBody()->getContents(), true);
+
+            $details = isset($data['error']['details']) ? $data['error']['details'] : [];
+            $message = isset($data['error']['message']) ? $data['error']['message'] : $e->getMessage();
+
+            if ($status == 401)
+                throw new AuthorizationException($status, $message);
+            else throw new GalaxPayRequestException($message, $status, $details);
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            $res = $e->getResponse();
+            throw new AuthorizationException($res->getStatusCode(), $res->getReasonPhrase());
+        } catch (\GuzzleHttp\Exception\ServerException $e) {
+            throw $e;
+        }
+    }
+}
